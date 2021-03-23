c3acf0e0590dafdcbee481b9a296da01f44acb81
Ben Fiedler <git@bfiedler.ch>
2021-03-23 23:35:17 +0100

    wip2

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -17,6 +16,7 @@ import (
 
 var n *nvim.Nvim
 var sb nvim.Buffer
+var wd nvim.Window
 
 type stub struct {
 	r io.Reader
@@ -104,6 +104,48 @@ func main() {
 	handleCommands(server, client)
 }
 
+func periodicUpdateCaret(s stream) {
+	b, err := n.CurrentBuffer()
+	if err != nil {
+		log.Fatal(err)
+	}
+	name, err := n.BufferName(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pos, err := n.WindowCursor(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var ft string
+	err = n.BufferOption(b, "filetype", &ft)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if ft != "isabelle" {
+		return
+	}
+	sendUpdateCaret(s, pos[0]-3, pos[1], name)
+}
+
+func sendUpdateCaret(s stream, line, col int, uri string) {
+	type params struct {
+		Uri  string `json:"uri"`
+		Line int    `json:"line"`
+		Col  int    `json:"character"`
+	}
+	n, err := jsonrpc2.NewNotification("PIDE/caret_update", params{Uri: uri, Line: line, Col: col})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("writing caret update")
+	_, err = s.s.Write(context.TODO(), n)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func toIsabelleHandler(s stream) func(nvim.Buffer, ...interface{}) {
 	return func(b nvim.Buffer, args ...interface{}) {
 		name, err := n.BufferName(b)
@@ -117,23 +159,48 @@ func toIsabelleHandler(s stream) func(nvim.Buffer, ...interface{}) {
 		}
 		log.Println("buffer: ", name)
 		log.Println("cursor: ", pos)
-		line := pos[0] - 1 // isabelle vscode_server is 0-indexed
+		line := pos[0] - 1 // isabelle vscode_server is 0-indexed, plus 2 extra for other stuff
 		col := pos[1]
+		sendUpdateCaret(s, line, col, "file://"+name)
+	}
+}
 
-		type params struct {
-			Uri  string `json:"uri"`
-			Line int    `json:"line"`
-			Col  int    `json:"character"`
-		}
-		n, err := jsonrpc2.NewNotification("PIDE/caret_update", params{Uri: fmt.Sprintf("file://%s", name), Line: line, Col: col})
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		log.Print("writing caret update")
-		_, err = s.s.Write(context.TODO(), n)
-		if err != nil {
-			log.Fatal(err)
+func proxyToIsabelleHandler(next proxy) proxy {
+	return func(to stream, msg jsonrpc2.Message) {
+		switch msg.(type) {
+		case *jsonrpc2.Notification:
+			next(to, msg)
+		case *jsonrpc2.Call:
+			c := msg.(*jsonrpc2.Call)
+			switch c.Method() {
+			case "textDocument/hover":
+				next(to, msg)
+				log.Println("got hover")
+				var params struct {
+					Position struct {
+						Line int `json:"line"`
+						Character int `json:"character"`
+					} `json:"position"`
+					TextDocument struct {
+						Uri string `json:"uri"`
+					}
+				}
+				j, err := c.Params().MarshalJSON()
+				if err != nil {
+					log.Fatal(err)
+				}
+				err = json.Unmarshal(j, &params)
+				if err != nil {
+					log.Fatal(err)
+				}
+				sendUpdateCaret(to, params.Position.Line, params.Position.Character, params.TextDocument.Uri)
+			default:
+				next(to, msg)
+			}
+		case *jsonrpc2.Response:
+			next(to, msg)
+		default:
+			log.Printf("unknown message type %T", msg)
 		}
 	}
 }
@@ -141,10 +208,11 @@ func toIsabelleHandler(s stream) func(nvim.Buffer, ...interface{}) {
 func proxyIsabelleHandler(next proxy) proxy {
 	return func(to stream, msg jsonrpc2.Message) {
 		switch msg.(type) {
-		case *jsonrpc2.Call:
-			c := msg.(*jsonrpc2.Call)
+		case *jsonrpc2.Notification:
+			c := msg.(*jsonrpc2.Notification)
 			switch c.Method() {
 			case "PIDE/dynamic_output":
+				log.Printf("got dynamic output")
 				// write into scratch buffer
 				var params struct {
 					Content string `json:"content"`
@@ -164,7 +232,7 @@ func proxyIsabelleHandler(next proxy) proxy {
 			default:
 				next(to, msg)
 			}
-		case *jsonrpc2.Notification:
+		case *jsonrpc2.Call:
 			next(to, msg)
 		case *jsonrpc2.Response:
 			next(to, msg)
@@ -196,12 +264,62 @@ func findOrCreateScratchBuf() nvim.Buffer {
 		log.Fatal(err)
 	}
 
+	err = n.SetBufferOption(b, "textwidth", 60)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	n.SetBufferName(b, "isabelle-output")
 	return b
 }
 
+func createWindow() nvim.Window {
+	w, err := n.CurrentWindow()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	height, err := n.WindowHeight(w)
+	if err != nil {
+		log.Fatal(err)
+	}
+	width, err := n.WindowWidth(w)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("width: %d", width)
+
+	var opts *nvim.WindowConfig
+	//if width > height {
+	opts = &nvim.WindowConfig{
+		Anchor:   "NW",
+		Relative: "win",
+		Height:   height - 2,
+		Width:    62,
+		Row:      0,
+		Col:      float64(width) - 70,
+	}
+	//} else {
+	//	*opts = nvim.WindowConfig{
+	//		Relative: "win",
+	//		Height: 15,
+	//		Width: 10,
+	//	}
+	//}
+
+	wd, err := n.OpenWindow(sb, false, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return wd
+}
+
 func handleCommands(vim, isa stream) {
 	sb = findOrCreateScratchBuf()
+	wd = createWindow()
+	log.Println("created temp buffer")
 
 	// TODO: attach to buffer create somehow
 	b, err := n.CurrentBuffer()
@@ -217,6 +335,14 @@ func handleCommands(vim, isa stream) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	err = n.RegisterHandler("CursorMoved_event", toIsabelleHandler(isa))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = n.Subscribe("CursorMoved")
+	if err != nil {
+		log.Fatal(err)
+	}
 	ok, err := n.AttachBuffer(b, true, map[string]interface{}{})
 	if !ok || err != nil {
 		log.Print(ok)
@@ -224,7 +350,7 @@ func handleCommands(vim, isa stream) {
 	}
 	log.Print("nvim setup done")
 
-	go runProxy(vim, isa, proxyLSP)
+	go runProxy(vim, isa, proxyToIsabelleHandler(proxyLSP))
 	runProxy(isa, vim, proxyIsabelleHandler(proxyLSP))
 }
 
